eca78f63a064724619c625ac8ed4e741e6d55883
Jerem <silva.jeremie93@gmail.com>
2023-03-10 16:10:16 +0100

    Dev day 6

--- __main__.py
+++ __main__.py
@@ -3,10 +3,11 @@ from models import (
     Tournament
 )
 
+
 def main_menu():
     input_choice = 0
     valid_choices: list[int] = [1, 2]
-    # TODO: ajouter les autres possibilitées dans le menu
+    # TODO: ajouter les autres possibilités dans le menu
 
     while int(input_choice) not in valid_choices:
         print("-------------------------------------")
@@ -26,6 +27,7 @@ def main_menu():
     print("-------------------------------------")
     bring_user_to_choice(input_choice)
 
+
 def bring_user_to_choice(input_choice: int):
     if input_choice == 1:
         new_player = Player()
@@ -34,8 +36,10 @@ def bring_user_to_choice(input_choice: int):
                 new_player.last_name,
                 new_player.birth_date):
             new_player.export_data_to_json_file()
+
     if input_choice == 2:
         new_tournament = Tournament()
+
 
 if __name__ == '__main__':
     main_menu()
--- controllers.py
+++ controllers.py
@@ -1,47 +1,59 @@
 import json
 import datetime
+from typing import Literal
+from models import Player
 
 
 class DataController:
-    def get_information_user(self, message: str, data_type: str = "string"):
+    def get_information_user(self, message: str, data_type: Literal["string", "day_date", "integer"] = "string"):
         """Ask the information from the user,
         print a custom message,
         checks the validity and return it
         """
         input_value: str = ""
+        input_year: str = ""
+        input_month: str = ""
+        input_day: str = ""
         while True:
-            # TODO: Utiliser la valeur Literal (import) t
             if data_type == "string":
                 try:
                     input_value = str(input(f"{message} : "))
+                    if input_value.isalpha() and len(input_value) < 100:
+                        break
                 except ValueError:
                     pass
-                if input_value.isalpha() and len(input_value) < 100:
-                    break
-            if data_type == "birth_date":
-                input_year = ""
-                input_month = ""
-                input_day = ""
+            elif data_type == "day_date":
+                print(f"{message} : ")
                 try:
-                    input_year = str(input("Année de naissance : "))
-                    input_month = str(input("Mois de naissance : "))
-                    input_day = str(input("Jour de naissance : "))
-                except:
-                    input_value = datetime(input_year, input_month, input_day)
-                # TODO: FINIR IMPORT DATETIME
-            else:
+                    input_year = str(input("Année : "))
+                    input_month = str(input("Mois : "))
+                    input_day = str(input("Jour : "))
+                    if (input_year.isdigit() and len(input_year) == 4 and
+                        input_month.isdigit() and len(input_month) == 2 and int(input_month) <= 12 and
+                        input_day.isdigit() and len(input_day) == 2 and int(input_day) <= 31
+                    ):
+                        input_value = str(datetime.date(int(input_year), int(input_month), int(input_day)))
+                        break
+                except ValueError:
+                    pass
+            elif data_type == "integer":
                 try:
                     input_value = int(input(f"{message} : "))
+                    if (str(input_value).isdigit() and
+                            len(str(input_value)) == 1 or len(str(input_value)) == 2
+                    ):
+                        break
                 except ValueError:
                     pass
-                if len(str(input_value)) == 1 or len(str(input_value)) == 2 and str(input_value).isdigit():
-                    break
+            else:
+                raise ValueError
             print(f"-Erreur- {message} invalide")
         return input_value
 
+
 class PlayerController(DataController):
 
-    def is_in_json_dataset(self, name: str, last_name: str, birth_date: int):
+    def is_in_json_dataset(self, name: str, last_name: str, birth_date: str):
         """Check with 3 parameters if the new player instance is already exist
          in the json dataset.
         """
@@ -71,13 +83,20 @@ class PlayerController(DataController):
         with open('Data/players.json', 'w') as json_player_file:
             json.dump(data_players, json_player_file)
 
+
 class TournamentController(DataController):
 
-    def generate_players_list(self):
-        # TODO: Poursuivre le DEV
-        pass
+    # def generate_players_list(self):
     #     players_list: list[Player] = []
     #     for i in range(0, self.players_count):
     #         player_to_add = Player()
     #         players_list.append(player_to_add)
     #     return players_list
+
+    def generate_players_list(self):
+        players_list = []
+        for i in range(0, self.players_count):
+            new_player = Player()
+            players_list.append(new_player)
+
+        return players_list
